4c371be69dd9eaa16373aba555269291fe345e00
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-05-19 23:29:29 +0530

    Exception Handling notes and examples

---
+++ Exception6.java
@@ -0,0 +1,24 @@
+package com.ktg.exceptions;//our class should extends exception,Throwable
+
+//parameterized constructor
+class AgeNotValid extends Exception {
+
+	public AgeNotValid(String message) {
+		super(message);
+	}
+}
+
+public class Exception6 {
+	static void validation(int age) throws AgeNotValid {
+		if (age < 18)
+
+			throw new AgeNotValid("You are not eligable to  vote : " + age);// MyException1 obj
+		else
+			System.out.println("you are eligable to vote");
+	}
+
+	public static void main(String args[]) throws AgeNotValid {
+		Exception6.validation(16);
+		System.out.println("rest of the code...");
+	}
+}
